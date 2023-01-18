b025451054e31e7a1d2478539e7130923c700018
rrirrirr <drpihl@gmail.com>
2023-01-18 20:37:10 +0100

    add return types

--- src/tournaments/tournaments.service.ts
+++ src/tournaments/tournaments.service.ts
@@ -4,11 +4,6 @@ import { UpdateTournamentDto } from './dto/update-tournament.dto'
 import { InjectConnection } from 'nest-knexjs'
 import { Knex } from 'knex'
 import { Tournament } from './entities/tournament.entity'
-import { User } from 'src/users/entities/user.entity'
-import { TournamentAdmin } from 'src/tournament_admins/entities/tournament_admin.entity'
-import { ForbiddenError } from '@casl/ability'
-import { Action } from 'src/abilities/action.enum'
-import { CaslAbilityFactory } from 'src/abilities/casl-ability.factory/casl-ability.factory'
 import { TournamentAdminsService } from 'src/tournament_admins/tournament_admins.service'
 import { AuthUser } from 'src/auth/authUser.entity'
 import { v4 as uuid } from 'uuid'
@@ -17,13 +12,14 @@ import { v4 as uuid } from 'uuid'
 export class TournamentsService {
   constructor(
     @InjectConnection() private readonly knex: Knex,
-    private readonly tournamentAdminsService: TournamentAdminsService,
-    private caslAbilityFactory: CaslAbilityFactory
+    private readonly tournamentAdminsService: TournamentAdminsService
   ) {}
 
-  async findAll(queries: CreateTournamentDto) {
+  async findAll(
+    queries: CreateTournamentDto
+  ): Promise<Tournament[] | undefined> {
     const tournaments = await this.knex.table('tournaments').where(queries)
-    return { tournaments }
+    return tournaments
   }
 
   async create(
@@ -40,22 +36,21 @@ export class TournamentsService {
         user_id: user.userId,
         tournament_id: tournament[0].id,
       }
-      const adminRight = this.tournamentAdminsService.create({
+      const adminRight = await this.tournamentAdminsService.create({
         id: uuid(),
         user_id: user.userId,
         tournament_id: tournament[0].id,
       })
     }
-
     return tournament[0]
   }
 
-  async findOne(id: number) {
+  async findOne(id: number): Promise<Tournament | undefined> {
     const tournament = await this.knex
       .table('tournaments')
       .select()
       .where({ id: id })
-    return tournament
+    return tournament[0]
   }
 
   async update(
@@ -63,15 +58,12 @@ export class TournamentsService {
     updateTournamentDto: UpdateTournamentDto,
     user: AuthUser
   ) {
-    const adminRights = await this.tournamentAdminsService.findByTournamentId(
-      id
-    )
-    const permission = adminRights.find(
-      (right) => right.user_id === user.userId
-    )
+    const permission = this.getPermission(id, user)
+
     if (!permission && !user.isAdmin) {
       throw new ForbiddenException('No permission')
     }
+
     const tournament = await this.knex
       .table('tournaments')
       .where({ id: id })
@@ -80,17 +72,23 @@ export class TournamentsService {
   }
 
   async remove(id: string, user: AuthUser) {
-    const adminRights = await this.tournamentAdminsService.findByTournamentId(
-      id
-    )
-    const permission = adminRights.find(
-      (right) => right.user_id === user.userId
-    )
+    const permission = this.getPermission(id, user)
+
     if (!permission && !user.isAdmin) {
       throw new ForbiddenException('No permission')
     }
 
     const res = await this.knex.table('tournaments').where({ id: id }).del()
     return res
+  }
+
+  private async getPermission(tournamentId: string, user: AuthUser) {
+    const adminRights = await this.tournamentAdminsService.findByTournamentId(
+      tournamentId
+    )
+    const permission = adminRights.find(
+      (right) => right.user_id === user.userId
+    )
+    return permission
   }
 }
