888806c02ab4f606289dba507289a2b1e865fc41
rrirrirr <drpihl@gmail.com>
2023-01-18 20:36:15 +0100

    add more subject types

--- src/abilities/casl-ability.factory/casl-ability.factory.ts
+++ src/abilities/casl-ability.factory/casl-ability.factory.ts
@@ -10,8 +10,26 @@ import { Action } from '../action.enum'
 import { Tournament } from 'src/tournaments/entities/tournament.entity'
 import { User } from 'src/users/entities/user.entity'
 import { TournamentAdmin } from 'src/tournament_admins/entities/tournament_admin.entity'
+import { GameType } from 'src/game_types/entities/game_type.entity'
+import { Message } from 'src/messages/entities/message.entity'
+import { Venue } from 'src/venues/entities/venue.entity'
+import { Invite } from 'src/invites/entities/invite.entity'
+import { Game } from 'src/games/entities/game.entity'
+import { Team } from 'src/teams/entities/team.entity'
 
-export type Subjects = InferSubjects<typeof Tournament | typeof User> | 'all'
+export type Subjects =
+  | InferSubjects<
+      | typeof Tournament
+      | typeof User
+      | typeof GameType
+      | typeof Message
+      | typeof Venue
+      | typeof Invite
+      | typeof Game
+      | typeof Team
+      | typeof TournamentAdmin
+    >
+  | 'all'
 
 export type AppAbility = Ability<[Action, Subjects]>
 
