459df09d8f11526ae7008a42296c139127c7f0e2
rrirrirr <drpihl@gmail.com>
2023-01-18 17:02:03 +0100

    add fields

--- src/tournaments/entities/tournament.entity.ts
+++ src/tournaments/entities/tournament.entity.ts
@@ -1,1 +1,12 @@
-export class Tournament {}
+export class Tournament {
+  id: string
+  name: string
+  format: string
+  max_num_teams: number
+  end_date: string
+  start_date: string
+  open: boolean
+  standings: any
+  game_type: string
+  description: string
+}
